039336f8005c98d06a94678807e9e5f1de72ca4d
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-06-18 12:25:06 -0400

    Splits razor component into partial class
    moves SignalR data to a new senor page

---
+++ PiServerApp.BlazorUI/Pages/Sensor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace PiServerApp.BlazorUI.Pages
+{
+    public partial class Sensor
+    {
+        
+        private HubConnection hubConnection;
+        private List<string> messages = new List<string>();
+        private string userInput;
+        private string messageInput;
+
+        protected override async Task OnInitializedAsync()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(NavigationManager.ToAbsoluteUri("/chathub"))
+                .Build();
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                var encodedMsg = $"{user}: {message}";
+                messages.Add(encodedMsg);
+                StateHasChanged();
+            });
+
+            hubConnection.On<string, int>("IntReceive", (user, message) =>
+            {
+                var encodedMsg = $"{user}: {message}";
+                messages.Add(encodedMsg);
+                StateHasChanged();
+            });
+
+            await hubConnection.StartAsync();
+        }
+
+        async Task Send() =>
+            await hubConnection.SendAsync("SendMessage", userInput, messageInput);
+
+        async Task SendInt() =>
+            await hubConnection.SendAsync("SendInt", userInput, messageInput);
+
+        public bool IsConnected =>
+            hubConnection.State == HubConnectionState.Connected;
+
+        public async ValueTask DisposeAsync()
+        {
+            await hubConnection.DisposeAsync();
+        }
+    }
+}
+
